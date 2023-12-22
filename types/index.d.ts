03303aeb18d08048abbcdb79d5821cc004227752
荣顶 <wangrongding@qq.com>
2023-12-22 09:44:01 +0800

    chore: 添加类型文件

---
+++ types/index.d.ts
@@ -0,0 +1,4 @@
+declare const Command: any;
+declare const version: any;
+declare const onUse: any, onList: any, onAdd: any, onTest: any;
+declare const program: any;
---
+++ types/main.d.ts
@@ -0,0 +1,4 @@
+export declare function onTest(name: string): void;
+export declare function onUse(name: string): void;
+export declare function onList(): void;
+export declare function onAdd(): void;
