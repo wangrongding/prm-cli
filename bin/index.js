0f13d68d4fd2b8962c21803779f8566c2be7908a
荣顶 <wangrongding.wrd@alibaba-inc.com>
2025-07-25 13:19:50 +0800

    chore: bin 添加 shebang

--- bin/index.js
+++ bin/index.js
@@ -1,36 +1,23 @@
-"use strict";
-const { Command } = require("commander");
-const version = require("../package.json").version;
-const { onUse, onList, onAdd, onTest, onDelete } = require("./main");
-const program = new Command();
+#!/usr/bin/env node
+
+'use strict'
+const { Command } = require('commander')
+const version = require('../package.json').version
+const { onUse, onList, onAdd, onTest, onDelete } = require('./main')
+const program = new Command()
 program
-    .name("prm")
-    .description("CLI to switching the registry of the package management tools")
-    .version(version, "-v, --version, -V");
+  .name('prm')
+  .description('CLI to switching the registry of the package management tools')
+  .version(version, '-v, --version, -V')
+program.command('list').alias('ls').description('List all the registries').action(onList)
+program.command('use <registry>').description('Change registry to registry').action(onUse)
+program.command('test [registry]').description('Show response time for specific or all registries').action(onTest)
 program
-    .command("list")
-    .alias("ls")
-    .description("List all the registries")
-    .action(onList);
-program
-    .command("use <registry>")
-    .description("Change registry to registry")
-    .action(onUse);
-program
-    .command("test [registry]")
-    .description("Show response time for specific or all registries")
-    .action(onTest);
-program
-    .command("add [name] [registry] [home]")
-    .description("Add a custom registry")
-    .action((name, registry, home) => {
-    onAdd(name, registry, home);
-});
-program
-    .command("delete <name>")
-    .alias("del")
-    .alias("rm")
-    .description("Delete a custom registry")
-    .action(onDelete);
-program.parse(process.argv);
+  .command('add [name] [registry] [home]')
+  .description('Add a custom registry')
+  .action((name, registry, home) => {
+    onAdd(name, registry, home)
+  })
+program.command('delete <name>').alias('del').alias('rm').description('Delete a custom registry').action(onDelete)
+program.parse(process.argv)
 //# sourceMappingURL=index.js.map
